38a15610eb63c440b84c77d5263d0e1c97a132d6
GadDev <alex.gadaix@gmail.com>
2026-04-08 23:14:39 +0200

    feat: add TranslocoPipe and LangSwitcher to header component imports

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,10 +1,12 @@
 import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { TranslocoPipe } from '@jsverse/transloco';
 import { ThemeToggle } from './theme-toggle';
+import { LangSwitcher } from '../lang-switcher/lang-switcher';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive, ThemeToggle],
+  imports: [RouterLink, RouterLinkActive, TranslocoPipe, ThemeToggle, LangSwitcher],
   templateUrl: './header.html',
   styleUrl: './header.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
